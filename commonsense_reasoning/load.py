511f57be22abd6bccef2d3bf2973e95bd42018c2
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-26 17:08:11 -0500

    Update load.py

--- commonsense_reasoning/load.py
+++ commonsense_reasoning/load.py
@@ -1,6 +1,12 @@
 import json
 
-with open('commonsense_170k.json') as f:
-    lines = json.loads(f)
+lines = []
+with open('commonsense_170k.json', 'r') as f:
+    for line in f:
+        try:
+            lines.append(json.loads(line.strip()))
+        except json.JSONDecodeError:
+            print(f"Error decoding line: {line}")
+            continue
 
 print(len(lines))
