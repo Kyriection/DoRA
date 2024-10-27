127ad6aa2a0b911095d58b194711b0a99b167242
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-26 19:41:39 -0500

    Update llama3_8B_DoRA.sh

--- commonsense_reasoning/llama3_8B_DoRA.sh
+++ commonsense_reasoning/llama3_8B_DoRA.sh
@@ -5,15 +5,28 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
+# CUDA_VISIBLE_DEVICES=$4 python finetune.py \
+#     --base_model 'meta-llama/Meta-Llama-3-8B' \
+#     --data_path 'commonsense_170k.json' \
+#     --output_dir $3 \
+#     --batch_size 16  --micro_batch_size 16 --num_epochs 3 \
+#     --learning_rate 1e-4 --cutoff_len 256 --val_set_size 120 \
+#     --eval_step 80 --save_step 80  --adapter_name dora \
+#     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
+#     --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing
+
 CUDA_VISIBLE_DEVICES=$4 python finetune.py \
     --base_model 'meta-llama/Meta-Llama-3-8B' \
     --data_path 'commonsense_170k.json' \
     --output_dir $3 \
     --batch_size 16  --micro_batch_size 16 --num_epochs 3 \
     --learning_rate 1e-4 --cutoff_len 256 --val_set_size 120 \
-    --eval_step 80 --save_step 80  --adapter_name dora \
+    --eval_step 80 --save_step 80  --adapter_name lora \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
     --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing
+
+
+
 
 # CUDA_VISIBLE_DEVICES=$4 python commonsense_evaluate.py \
 #     --model LLaMA3-8B \
