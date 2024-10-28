e39d6f5c38a0d940871dbffac1bb258189ef9a8d
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-27 22:07:10 -0500

    Update LoRA.sh

--- commonsense_reasoning/script/LoRA.sh
+++ commonsense_reasoning/script/LoRA.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=$1 python finetune.py \
     --base_model 'meta-llama/Llama-3.2-3B' \
     --data_path 'commonsense_170k.json' \
     --output_dir $2 \
-    --batch_size 16  --micro_batch_size 1 --num_epochs 3 \
+    --batch_size 16  --micro_batch_size 16 --num_epochs 3 \
     --learning_rate 3e-4 --cutoff_len 256 --val_set_size 120 \
     --eval_step 80 --save_step 80  --adapter_name lora \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=$1 python finetune.py \
 for task in boolq piqa social_i_qa hellaswag winogrande ARC-Challenge ARC-Easy openbookqa; do
 CUDA_VISIBLE_DEVICES=$1 python commonsense_evaluate.py \
     --model LLaMA3-8B \
-    --adapter lora \
+    --adapter LoRA \
     --dataset ${task} \
     --base_model 'meta-llama/Llama-3.2-3B' \
     --batch_size 1 \
