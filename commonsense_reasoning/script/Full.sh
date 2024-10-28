f41e40802d2507930bf9c63a72b1d9966c71b90d
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-27 22:39:06 -0500

    test

--- commonsense_reasoning/script/Full.sh
+++ commonsense_reasoning/script/Full.sh
@@ -6,33 +6,6 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-
-# CUDA_VISIBLE_DEVICES=$1 python finetune.py \
-#     --base_model 'meta-llama/Meta-Llama-3-8B' \
-#     --data_path 'commonsense_170k.json' \
-#     --output_dir $2 \
-#     --batch_size 16  --micro_batch_size 1 --num_epochs 3 \
-#     --learning_rate 3e-4 --cutoff_len 256 --val_set_size 120 \
-#     --eval_step 80 --save_step 80  --adapter_name lora \
-#     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
-#     --lora_r 32 --lora_alpha 64
-
-
-
-
-
-
-# for task in boolq piqa social_i_qa hellaswag winogrande ARC-Challenge ARC-Easy openbookqa; do
-# CUDA_VISIBLE_DEVICES=$1 python commonsense_evaluate.py \
-#     --model LLaMA3-8B \
-#     --adapter lora \
-#     --dataset ${task} \
-#     --base_model 'meta-llama/Meta-Llama-3-8B' \
-#     --batch_size 1 \
-#     --lora_weights $2|tee -a $2/${task}.txt
-# done
-
-
 CUDA_VISIBLE_DEVICES=$1 python -u full_finetune.py \
     --base_model 'meta-llama/Llama-3.2-3B' \
     --data_path 'commonsense_170k.json' \
