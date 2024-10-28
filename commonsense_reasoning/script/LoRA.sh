fb2da1284da503b57b5912b2d0221c131acdcfb1
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-27 21:59:54 -0500

    Update LoRA.sh

--- commonsense_reasoning/script/LoRA.sh
+++ commonsense_reasoning/script/LoRA.sh
@@ -8,7 +8,7 @@
 
 
 CUDA_VISIBLE_DEVICES=$1 python finetune.py \
-    --base_model 'meta-llama/Meta-Llama-3-8B' \
+    --base_model 'meta-llama/Llama-3.2-3B' \
     --data_path 'commonsense_170k.json' \
     --output_dir $2 \
     --batch_size 16  --micro_batch_size 1 --num_epochs 3 \
@@ -22,7 +22,7 @@ CUDA_VISIBLE_DEVICES=$1 python commonsense_evaluate.py \
     --model LLaMA3-8B \
     --adapter lora \
     --dataset ${task} \
-    --base_model 'meta-llama/Meta-Llama-3-8B' \
+    --base_model 'meta-llama/Llama-3.2-3B' \
     --batch_size 1 \
     --lora_weights $2|tee -a $2/${task}.txt
 done
