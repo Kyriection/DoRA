64da7544552236bdc28847ab2578f78e01e580fa
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-28 23:54:59 -0500

    Update APOLLO_mini.sh

--- commonsense_reasoning/script/1b/APOLLO_mini.sh
+++ commonsense_reasoning/script/1b/APOLLO_mini.sh
@@ -3,10 +3,10 @@ CUDA_VISIBLE_DEVICES=$1 python -u full_finetune.py \
     --data_path 'commonsense_170k.json' \
     --output_dir $2 \
     --batch_size 32 --micro_batch_size 32 --num_epochs 3 \
-    --learning_rate 1e-4 --cutoff_len 256 --val_set_size 120 \
+    --learning_rate $4 --cutoff_len 256 --val_set_size 120 \
     --eval_step 80 --save_step 80 \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
-    --optimizer_name appollo_tensor --galore_rank 1 --galore_scale 128 
+    --optimizer_name appollo_tensor --galore_rank 1 --galore_scale $3 
 
 for task in boolq piqa social_i_qa hellaswag winogrande ARC-Challenge ARC-Easy openbookqa; do
 CUDA_VISIBLE_DEVICES=$1 python full_evaluate.py \
