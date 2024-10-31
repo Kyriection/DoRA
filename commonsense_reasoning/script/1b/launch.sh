2d17bb1ddd9d9384a6b1b0f33fbe0c2854da4ab9
Allen <39230030+Kyriection@users.noreply.github.com>
2024-10-30 20:38:47 -0500

    Update launch.sh

--- commonsense_reasoning/script/1b/launch.sh
+++ commonsense_reasoning/script/1b/launch.sh
@@ -9,4 +9,7 @@ nohup bash script/1b/APOLLO_SVD.sh 3 apollo_svd 5e-4 > log_apollo_svd_5e-4.txt 2
 
 
 
-
+nohup bash script/1b/APOLLO_mini.sh 0 3e-4 64 > log_apollo_mini_64_3e-4.txt 2>&1 &
+nohup bash script/1b/APOLLO_mini.sh 1 2e-4 128 > log_apollo_mini_128_2e-4.txt 2>&1 &
+nohup bash script/1b/APOLLO_mini.sh 2 5e-4 128 > log_apollo_mini_128_5e-4.txt 2>&1 &
+nohup bash script/1b/APOLLO_mini.sh 3 1e-4 128  > log_apollo_mini_128_1e-4.txt 2>&1 &
