2cec9329c7b6c5b385e1a3d87ef0af57c6b376ab
Allen <39230030+Kyriection@users.noreply.github.com>
2024-11-08 11:28:29 -0600

    Update merge.py

--- commonsense_reasoning/merge.py
+++ commonsense_reasoning/merge.py
@@ -18,7 +18,17 @@ model = PeftModel.from_pretrained(
         model,
         lora_weights,
         torch_dtype=torch.float16,
-        device_map={"":0}
-    )
+        device_map={"":0})
+
+
+for name, param in model.named_parameters():
+    if "lora" in name:
+        import pdb; pdb.set_trace()
+        main_param_name = name.replace("lora_", "")
+        if hasattr(model, main_param_name):
+            main_param = getattr(model, main_param_name)
+            with torch.no_grad():
+                main_param += param
+
 
 import pdb; pdb.set_trace()
