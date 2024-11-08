52e34eb97038af1e6b1a0e93e05094949d34c45d
Allen <39230030+Kyriection@users.noreply.github.com>
2024-11-07 23:43:24 -0600

    Update lora.py

--- commonsense_reasoning/peft/src/peft/tuners/lora.py
+++ commonsense_reasoning/peft/src/peft/tuners/lora.py
@@ -185,7 +185,10 @@ class LoraModel(torch.nn.Module):
                             )
                             kwargs["fan_in_fan_out"] = self.peft_config.fan_in_fan_out = False
                     new_module = MergedLinear(in_features, out_features, bias=bias, **kwargs)
-                print(target, isinstance(target, R_Sparse_Linear), self.peft_config.enable_lora)
+                else:
+                    print('Lora with R_Sparse_Linear', target)
+                    new_module = R_Sparse_Linear(target.in_features, target.out_features, bias=bias, **kwargs)
+
                 self._replace_module(parent, target_name, new_module, target)
         if not is_target_modules_in_base_model:
             raise ValueError(
