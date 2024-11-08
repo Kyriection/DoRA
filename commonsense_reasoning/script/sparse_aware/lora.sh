f877e460afb801e7e284bd7de649dbf10c975441
Allen <39230030+Kyriection@users.noreply.github.com>
2024-11-07 23:55:04 -0600

    Update lora.sh

--- commonsense_reasoning/script/sparse_aware/lora.sh
+++ commonsense_reasoning/script/sparse_aware/lora.sh
@@ -11,7 +11,7 @@ CUDA_VISIBLE_DEVICES=$1 python finetune_sparse.py \
     --base_model 'meta-llama/Llama-2-7b-hf' \
     --data_path 'commonsense_15k.json' \
     --output_dir $2 \
-    --batch_size 16  --micro_batch_size 1 --num_epochs 3 \
+    --batch_size 16  --micro_batch_size 8 --num_epochs 3 \
     --learning_rate 3e-4 --cutoff_len 256 --val_set_size 120 \
     --eval_step 80 --save_step 80  --adapter_name lora \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
