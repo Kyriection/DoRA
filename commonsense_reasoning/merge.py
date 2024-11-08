5768a482dfc623824efeeb598f2d2e099cf588f9
Allen <39230030+Kyriection@users.noreply.github.com>
2024-11-08 11:41:12 -0600

    Update merge.py

--- commonsense_reasoning/merge.py
+++ commonsense_reasoning/merge.py
@@ -14,21 +14,21 @@ model = AutoModelForCausalLM.from_pretrained(
         device_map="auto",
         trust_remote_code=True)
 
-model = PeftModel.from_pretrained(
+peft_model = PeftModel.from_pretrained(
         model,
         lora_weights,
         torch_dtype=torch.float16,
         device_map={"":0})
 
-
-for name, param in model.named_parameters():
-    if "lora" in name:
+checkpoints = {}
+for name, module in peft_model.named_modules():
+    if hasattr(module, "lora_A") and hasattr(module, "lora_B"):
         import pdb; pdb.set_trace()
-        main_param_name = name.replace("lora_", "")
-        if hasattr(model, main_param_name):
-            main_param = getattr(model, main_param_name)
-            with torch.no_grad():
-                main_param += param
+        lora_weight = module.lora_B.weight @ module.lora_A.weight * module.scaling
+        checkpoints[name+'.weight'] = module.weight.data + lora_weight
 
+for name, p in model.named_parameters():
+    if name in checkpoints:
+        p.data = checkpoints[name]
 
-import pdb; pdb.set_trace()
+torch.save(model.state_dict(), "llama2_7b_ft_50sparsity/pytorch_model.bin")
